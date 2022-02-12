3d0423095c7aef9b4d2ee282e752e514f79ca378
Ahtasham Ali <ahtashamjaved@yahoo.com>
2022-02-12 18:19:48 +0100

    updated

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,6 +1,93 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// -------- React Lists ----------------------
+
+
+function Car(props) {
+  return <li>I am a { props.brand }</li>;
+}
+
+function Garage() {
+  const cars = [
+    {id: 1, brand: 'Ford'},
+    {id: 2, brand: 'BMW'},
+    {id: 3, brand: 'Audi'}
+  ];
+  return (
+    <>
+      <h1>Who lives in my garage?</h1>
+      <ul>
+        {cars.map((car) => <Car key={car.id} brand={car.brand} />)}
+      </ul>
+    </>
+  );
+}
+
+ReactDOM.render(<Garage />, document.getElementById('root'));
+
+/*
+function Car(props) {
+  return <li>I am a { props.brand }</li>;
+}
+
+function Garage() {
+  const cars = ['Ford', 'BMW', "Audi"];
+  return (
+    <>
+    <h1>Who lives in my garage?</h1>
+    <ul>
+      {cars.map((car) => <Car brand= {car} />)}
+    </ul>
+    </>
+  );
+}
+
+ReactDOM.render(<Garage />, document.getElementById('root'));
+// -------- Logical && Operator --------------
+/*
+function Garage(props) {
+  const cars = props.cars;
+  return (
+    <>
+    <h1>Garage</h1>
+    {cars.length > 0 &&
+    <h2>
+      You have {cars.length} cars in your garage.
+    </h2>
+    }
+    </>
+  );
+    
+  }
+  const cars = ['Ford', 'BMW', 'Audi'];
+  ReactDOM.render(<Garage cars = {cars} />,
+    document.getElementById('root')
+    );
+
+// -------- React if ---------------
+/*
+function MissedGoal() {
+  return <h1>Missed</h1>;
+}
+
+function MadeGoal() {
+  return <h1>Goal!</h1>
+}
+
+function Goal(props) {
+  const isGoal = props.isGoal;
+  if(isGoal) {
+    return <MadeGoal/>
+  }
+  return <MissedGoal />
+}
+ReactDOM.render(
+  <Goal isGoal = {false} />, document.getElementById('root')
+);
+
+// -------- Components --------------------
+/*
 class Car extends React.Component {
   constructor(props) {
     super(props);
